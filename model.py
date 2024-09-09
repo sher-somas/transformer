4c28a140dadb0217a6cc61d4d9ccd54fa7cb33e0
sher.somas <sher.somas@gmail.com>
2024-09-09 23:01:59 +0200

    adding input embeddings

---
+++ model.py
@@ -0,0 +1,17 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, model_dimension: int, vocab_size: int):
+
+        super().__init__()
+        self.model_dimension = model_dimension
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, model_dimension)
+
+    def forward(self, x):
+        return self.embedding * math.sqrt(self.model_dimension)
