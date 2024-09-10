5310a9284bcc876e02d6374a4c4f99537258de9a
sher.somas <sher.somas@gmail.com>
2024-09-10 16:51:40 +0200

    added residual connections and EncoderBlock

--- model.py
+++ model.py
@@ -150,3 +150,27 @@ class MultiHeadAttentionBlock(nn.Module):
         # (Batch, seq_len, model_dimension) --> (Batch, seq_len, model_dimension)
         return self.W_o(x)
 
+class ResidualConnection(nn.Module):
+
+    def __init__(self, dropout: float) -> None: 
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x , previous_layer):
+        return x + self.dropout(previous_layer(self.norm(x)))
+
+
+class EncoderBlock(nn.Module):
+
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x , x, src_mask)) # x is the query, key, value
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+    
